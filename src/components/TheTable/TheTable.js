3e0648b26cc411940da843d5d1e332ce5893bc63
IvoTsochev <ivo.tsochev@live.com>
2021-04-05 21:26:49 +0300

    fixing the loading animation

--- src/components/TheTable/TheTable.js
+++ src/components/TheTable/TheTable.js
@@ -14,27 +14,21 @@ const TheTable = () => {
 
     // Fetching data
     useEffect(() => {
-
         axios.get('https://services.odata.org/TripPinRESTierService/(S(3jgtctz5a2wyzb0gi3pxikvb))/People')
             .then(res => {
                 const tableData = res.data.value;
-                setTableData(tableData)
+                setTableData(tableData);
+                setIsLoaded(true);
             })
     }, [])
 
-
-    const columns = useMemo(() => COLUMNS, []);
-    const data = useMemo(() => tableData, []);
-
-
-
-
+    const columns = useMemo(() => COLUMNS, [tableData]);
+    const data = useMemo(() => tableData, [tableData]);
 
     const tableInstance = useTable({
         columns,
         data
     })
-
 
     const {
         getTableProps,
@@ -45,46 +39,55 @@ const TheTable = () => {
     } = tableInstance;
 
     return (
-        <table {...getTableProps()}>
-            <thead>
-                {
-                    headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {
-                                headerGroup.headers.map(column => (
-                                    <th {...column.getHeaderProps()} >
-                                        {column.render('Header')}
-                                    </th>
-                                ))
-                            }
+        <>
+            {isLoaded ?
+                <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                :
+                <table {...getTableProps()}>
 
-                        </tr>
-                    ))
-                }
+                    <thead>
+                        {
+                            headerGroups.map(headerGroup => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {
+                                        headerGroup.headers.map(column => (
+                                            <th {...column.getHeaderProps()} >
+                                                {column.render('Header')}
+                                            </th>
+                                        ))
+                                    }
 
-            </thead>
+                                </tr>
+                            ))
+                        }
 
-            <tbody {...getTableBodyProps()}>
-                {
-                    rows.map(row => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {
-                                    row.cells.map(cell => {
-                                        return (
-                                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                        )
-                                    })
-                                }
+                    </thead>
 
-                            </tr>
-                        )
-                    })
-                }
+                    <tbody {...getTableBodyProps()}>
+                        {
+                            rows.map(row => {
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {
+                                            row.cells.map(cell => {
+                                                return (
+                                                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                                )
+                                            })
+                                        }
 
-            </tbody>
-        </table>
+                                    </tr>
+                                )
+                            })
+                        }
+
+                    </tbody>
+                </table>
+            }
+
+        </>
+
     )
 }
 
